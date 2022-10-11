f41b32d803abec2c8b7e338ead41d7c6be26dbf3
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-10-11 15:35:16 -0400

    css fix

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -14,11 +14,25 @@ const Footer = () => {
                 <a href="https://www.facebook.com/Back-Home-110748035078931">
                     <button className='social_network' type="button" >  </button>
                 </a>
-                <button className='social_network2'></button>
-                <button className='social_network3'></button>
+
+                <a href="https://www.instagram.com/sir._.robot/">
+                    <button className='social_network2' type="button" >  </button>
+                </a>
+
+                <a href="https://www.linkedin.com/in/alantmatos/">
+                    <button className='social_network3' type="button" >  </button>
+                </a>
+
+
             </div>
         </div>
     );
 }
 
 export default Footer;
+
+
+
+
+
+
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import banner from '../Assets/banner.png'
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import style from '../css/Navbar.css'
 
 const Navbar = ({ user, setUser }) => {
 
@@ -21,54 +22,48 @@ const Navbar = ({ user, setUser }) => {
 
 
 
-    return (
+    return (<>
 
         <div className='navbar_container'>
 
-            <div className='user_container'>
-                <div className='user_card'>
-                    <div className='user_header'>
-                        <div className='user_img'>
-                            {user ? <img src={user.img_url} className="img_size" alt="User profile"></img> : null}
+            <div className='user-brand'>
+
+                {user ?
+                    <div className='user_card'>
+                        <div className='user_header'>
+                            <div className='user_img'>
+                                <img src={user.img_url} className="img_size" alt="User profile"></img>
+                            </div>
+                            <div className='user_name'>
+                                <h1>Hi, {user.name}.</h1>
+                            </div>
                         </div>
-                        <div className='user_name'>
-                            <h1>{user ? `Hi, ${user.name}.` : null}</h1>
+
+                        <div className='user_info'>
+                            <p> Phone Number: {user.phone}.</p>
+                            <p> E-mail: {user.e_mail}.</p>
+                            <p> Location: {user.city_state}.</p>
                         </div>
-                    </div>
 
-
-
-                    <div className='user_info'>
-                        <p> {user ? `Phone Number: ${user.phone}.` : ''}</p>
-                        <p> {user ? `E-mail: ${user.e_mail}.` : ''}</p>
-                        <p> {user ? `City_State ${user.city_state}.` : ''}</p>
-
-                        {user ? <div className='user_btn'>
+                        <div className='user_btn'>
                             <Link to='/editprofile' className='user_buttons'> Edit profile</Link>
                             <Link to='/addapet' className='user_buttons'> Add a pet</Link>
-                        </div> : ""}
+                        </div>
+
                     </div>
+                    : null}
 
 
-
+                <div className='title_container'>
+                    <img className='navbar_img' src={banner} alt='banner'></img>
+                    <h1 className='title'>Back Home</h1>
                 </div>
 
-
-
-
             </div>
 
+        </div>
 
-
-
-
-
-
-            <div className='title_container'>
-                <img className='navbar_img' src={banner} alt='banner'></img>
-                <h1 className='title'>Back Home</h1>
-
-            </div>
+        <div className='nav_btn'>
 
             <ul className='nav_ul'>
                 <li>
@@ -84,16 +79,12 @@ const Navbar = ({ user, setUser }) => {
                 </li>
 
                 <li>
-                    <Link to='/profile' className='nav_buttons'> Profile</Link>
+                    <Link to='/profile' className='nav_buttons'> My Pets</Link>
                 </li>
             </ul>
-
-
-
-
         </div>
 
-    );
+    </>);
 }
 
 export default Navbar;
